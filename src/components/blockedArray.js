95965b8dff77a6b95c724e22094729800f6d562a
Anthony-Rondina <anthony.rondina@icloud.com>
2022-07-01 19:20:59 -0400

    updating lightsaber colors

--- src/components/blockedArray.js
+++ src/components/blockedArray.js
@@ -1,23 +1,28 @@
+import styles from "./blockedArray.module.css"
 export default function blockedArray({ blockedArr, handleUnblock }) {
     return (
-        <>
-            <aside>Blocked Content</aside>
-            {blockedArr.map((blockedName, index) => {
-                return (
-                    <div key={index}>
-                        <div style={{ color: "red" }}>
-                            {blockedName}
-                            <button
-                                onClick={() => {
-                                    handleUnblock(blockedName);
-                                }}
-                            >
-                                Unblock
-                            </button>
-                        </div>
-                    </div>
-                );
-            })}
-        </>
+        <aside className={styles.blockedList}>
+            <p>Blocked Content</p>
+            <div className={styles.results}>
+                {
+                    blockedArr.map((blockedName, index) => {
+                        return (
+                            <div className={styles.options} key={index}>
+                                <div style={{ color: "red" }}>
+                                    <p>{blockedName}</p>
+                                    <button
+                                        onClick={() => {
+                                            handleUnblock(blockedName);
+                                        }}
+                                    >
+                                        Unblock
+                                    </button>
+                                </div>
+                            </div>
+                        );
+                    })
+                }
+            </div>
+        </aside >
     );
 }
